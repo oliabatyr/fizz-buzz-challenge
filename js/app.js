93c106a451a266899747cf0464afc053fbba9358
oliabatyr <oliabatyr@gmail.com>
2016-08-23 18:01:44 -0400

    Final commit

--- js/app.js
+++ js/app.js
@@ -1,14 +1,18 @@
 for (var i=1; i<=100; i++){
   if (i % 15 == 0){
     console.log('Fizz Buzz');
+    document.write('Fizz Buzz');
   }
   else if (i % 5 == 0){
     console.log('Buzz');
+    document.write('Buzz');
   }
   else if (i % 3 == 0){
     console.log('Fizz');
+    document.write('Fizz');
   }
   else {
     console.log(i);
+    document.write(i);
   }
 }
